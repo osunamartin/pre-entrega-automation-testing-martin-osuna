f796f0ff1799a655a9c5b0e77ed3f683834391a5
Martin Osuna <martin.osuna13@gmail.com>
2025-10-14 12:00:08 -0300

    comprobación carrito (contador y producto)

--- automatizacion_login.py
+++ automatizacion_login.py
@@ -30,13 +30,21 @@ try:
  first_price = products[0].find_element(By.CSS_SELECTOR, ".inventory_item_price").text
  print(f"Primer producto → {first_name} – {first_price}")
 
- # 5) Agregar un producto al carrito
+ # 5) Agregar un producto al carrito y comprobar que esté. 
  driver.find_element(By.ID, 'add-to-cart-sauce-labs-backpack').click()
  print(f"{first_name} agregado al carrito")
+ driver.find_element(By.ID, 'shopping_cart_container').click() #ir al carrito 
+ product_name = driver.find_element(By.CLASS_NAME, 'inventory_item_name').text
+ assert first_name == product_name
+ 
+ #comprobar que el contador del carrito suba/baje dependiendo la cantidad de productos agregados
+ items_in_cart = driver.find_elements(By.CLASS_NAME, "cart_item")
+ cart_count = int(driver.find_element(By.CLASS_NAME, "shopping_cart_badge").text)
+ assert cart_count == len(items_in_cart)
 
  # 6) Reiniciar al estado original de la web
  driver.find_element(By.ID, 'react-burger-menu-btn').click()
- driver.implicitly_wait(3)
+ time.sleep(1) #Espera a que el click de arriba despliegue el menú para que aparezca el elemento.
  driver.find_element(By.ID, 'reset_sidebar_link').click()
  
 finally:
